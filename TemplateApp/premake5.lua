b96df0b898844a1a65040cac4ac3717e3363322c
Erik <92889691+erikrl2@users.noreply.github.com>
2022-10-06 15:09:24 +0200

    Initial commit

---
+++ TemplateApp/premake5.lua
@@ -0,0 +1,72 @@
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "TemplateApp" -- TODO: Rename
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+	}
+
+	defines
+	{
+		"SFML_STATIC"
+	}
+
+	includedirs
+	{
+		"src",
+		"vendor/SFML-2.5.1/include"
+	}
+
+	libdirs
+	{
+		"vendor/SFML-2.5.1/lib"
+	}
+
+	links
+	{
+		"opengl32.lib",
+		"winmm.lib",
+		"gdi32.lib",
+		"freetype.lib",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+
+		defines { "DEBUG" }
+
+		links
+		{
+			"sfml-system-s-d.lib",
+			"sfml-window-s-d.lib",
+			"sfml-graphics-s-d.lib",
+			"sfml-audio-s-d.lib",
+			"sfml-network-s-d.lib"
+		}
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "On"
+
+		defines { "NDEBUG" }
+
+		links
+		{
+			"sfml-system-s.lib",
+			"sfml-window-s.lib",
+			"sfml-graphics-s.lib",
+			"sfml-audio-s.lib",
+			"sfml-network-s.lib"
+		}
